0d40c0132f30e72e14aeb95c36f28b5d5f2bffd2
Thibault Durand <tibo.durand@gmail.com>
2019-05-29 19:18:43 -0300

    Evo: working location panTo

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,5 +1,9 @@
 import { Component, h } from '@stencil/core';
 import mapboxgl from 'mapbox-gl';
+import { Plugins } from '@capacitor/core';
+
+const { Geolocation } = Plugins;
+
 
 @Component({
   tag: 'app-home',
@@ -8,14 +12,37 @@ import mapboxgl from 'mapbox-gl';
 export class AppHome {
 
   componentDidLoad() {
+    
     mapboxgl.accessToken = 'pk.eyJ1IjoidGR1cmFuZCIsImEiOiJjampsN2p0ZjkwOG5sM3BwY2o5a3lhcHhlIn0.UUKhPTCWbivcEScbhWIXJg';
+
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v9'
+      style: 'mapbox://styles/mapbox/streets-v9',
+      zoom: 17
     });
+
+    // Watch position not working on android device
+    // Geolocation.watchPosition({enableHighAccuracy: true}, (result) => {
+    //   console.log('Blabalbal')
+    //   if (result && result.coords) {
+    //     console.log(result);
+    //   }
+    // })
+
+    Geolocation.getCurrentPosition().then((position) => {
+      // console.log('coords');
+      if(position) {
+        // console.log(position.coords);
+        map.panTo({lon: position.coords.longitude, lat: position.coords.latitude})
+      }
+      
+    });
+
+
     map.on('load',() => {
       map.resize();
     });
+    
   }
 
   render() {

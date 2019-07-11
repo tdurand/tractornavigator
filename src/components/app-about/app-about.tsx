d213667d7d036ed46b9f554ee735832e1235a29c
Thibault Durand <tibo.durand@gmail.com>
2019-07-11 19:45:08 +0200

    WIP: add more in about page

--- src/components/app-about/app-about.tsx
+++ src/components/app-about/app-about.tsx
@@ -20,6 +20,10 @@ export class AppAbout {
       <ion-content class="ion-padding">
         <img src="/assets/logo_gsa.svg" />
         <p>This app is developed as part of the <a href="https://www.gsa.europa.eu/mygalileoapp">MyGalileoApp Challenge 2019</a></p>
+
+        <p>It is developed as an Opensource project (license MIT)</p>
+        <p><a href="https://github.com/tdurand/tractornavigator">https://github.com/tdurand/tractornavigator</a></p>
+
         <p class="text-bold">Team</p>
         <ul>
           <li>Vincent Duret</li>

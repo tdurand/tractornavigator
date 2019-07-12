841291085330a35d2a6e315c6be11758c3c2f13a
Thibault Durand <tibo.durand@gmail.com>
2019-07-12 09:59:03 +0200

    Evo: watch position working

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -2,9 +2,11 @@ import { Component, h, State } from '@stencil/core';
 import mapboxgl from 'mapbox-gl';
 import { Plugins, GeolocationPosition } from '@capacitor/core';
 import config from '../../config.json';
+import pointGeojson from '../../assets/geojson/point.json';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 import { toastController } from '@ionic/core';
 const { SplashScreen } = Plugins;
+import { PulsingDot } from '../../helpers/utils';
 
 /*
 
@@ -21,25 +23,24 @@ const { Geolocation } = Plugins;
 export class AppHome {
 
   @State() position: GeolocationPosition;
+  @State() positionGeojson: any = pointGeojson;
 
-  async presentToastNavigationM2() {  
-    const toast = await toastController.create({
-      message: "Isn't ready yet, working on it, check other pages ;-)",
-      color: 'dark',
-      position: 'top',
-      buttons: [
-        {
-          text: 'OK',
-          role: 'cancel'
-        }
-      ]
+  createPositionLayerAndSource(map) {
+    let pulsingDot = new PulsingDot(map);
+    map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+    map.addSource('position' , { type: 'geojson', data: this.positionGeojson})
+    map.addLayer({
+      "id": "position",
+      "type": "symbol",
+      "source": "position",
+      "layout": {
+        "icon-image": "pulsing-dot"
+      }
     });
-  
-    return await toast.present();
   }
 
-  componentDidRender() {
-    this.presentToastNavigationM2();
+  updatePositionInSource(map) {
+    map.getSource('position').setData(this.positionGeojson);
   }
 
   componentDidLoad() {
@@ -62,99 +63,36 @@ export class AppHome {
       console.log('blabla')
     });
 
-    var size = 100;
-
-    var pulsingDot = {
-      width: size,
-      height: size,
-      data: new Uint8Array(size * size * 4),
-
-      onAdd: function () {
-        var canvas = document.createElement('canvas');
-        canvas.width = this.width;
-        canvas.height = this.height;
-        this.context = canvas.getContext('2d');
-      },
-
-      render: function () {
-        var duration = 1000;
-        var t = (performance.now() % duration) / duration;
-
-        var radius = size / 2 * 0.3;
-        var outerRadius = size / 2 * 0.7 * t + radius;
-        var context = this.context;
-
-        // draw outer circle
-        context.clearRect(0, 0, this.width, this.height);
-        context.beginPath();
-        context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
-        context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
-        context.fill();
-
-        // draw inner circle
-        context.beginPath();
-        context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
-        context.fillStyle = 'rgba(255, 100, 100, 1)';
-        context.strokeStyle = 'white';
-        context.lineWidth = 2 + 4 * (1 - t);
-        context.fill();
-        context.stroke();
-
-        // update this image's data with data from the canvas
-        this.data = context.getImageData(0, 0, this.width, this.height).data;
-
-        // keep the map repainting
-        map.triggerRepaint();
-
-        // return `true` to let the map know that the image was updated
-        return true;
-      }
-    };
-
-    // Watch position not working on android device
-    // Geolocation.watchPosition({enableHighAccuracy: true}, (result) => {
-    //   console.log('Blabalbal')
-    //   if (result && result.coords) {
-    //     console.log(result);
-    //   }
-    // })
-
     Geolocation.getCurrentPosition().then((position) => {
-      // console.log('coords');
       if (position) {
         this.position = position;
         map.panTo({ lon: position.coords.longitude, lat: position.coords.latitude })
       }
 
+      Geolocation.watchPosition({
+        enableHighAccuracy: true
+      }, (position) => {
+        if (position) {
+          this.position = position;
+          // Update layer
+          this.positionGeojson.features[0].geometry.coordinates = [this.position.coords.longitude, this.position.coords.latitude];
+          if (!map.getSource("position")) {
+            this.createPositionLayerAndSource(map);
+          }
+          this.updatePositionInSource(map);
+        }
+      })
     });
 
 
     map.on('load', () => {
       map.resize();
+      this.positionGeojson.features[0].geometry.coordinates = [this.position.coords.longitude, this.position.coords.latitude];
 
-
-      map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
-
-      map.addLayer({
-        "id": "points",
-        "type": "symbol",
-        "source": {
-          "type": "geojson",
-          "data": {
-            "type": "FeatureCollection",
-            "features": [{
-              "type": "Feature",
-              "geometry": {
-                "type": "Point",
-                "coordinates": [this.position.coords.longitude, this.position.coords.latitude]
-              }
-            }]
-          }
-        },
-        "layout": {
-          "icon-image": "pulsing-dot"
-        }
-      });
+      if (!map.getSource("position")) {
+        this.createPositionLayerAndSource(map);
+      }
+      this.updatePositionInSource(map);
     });
   }
 
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -3,3 +3,58 @@
 export function sayHello() {
   return Math.random() < 0.5 ? 'Hello' : 'Hola';
 }
+
+export class PulsingDot {
+
+  size = 100;
+  width = 100;
+  height = 100;
+  data = new Uint8Array(100 * 100 * 4);
+  map =  null;
+  context = null;
+
+  constructor(map) {
+    this.map = map;
+  }
+
+  onAdd() {
+    var canvas = document.createElement('canvas');
+    canvas.width = this.width;
+    canvas.height = this.height;
+    this.context = canvas.getContext('2d');
+  }
+
+  render() {
+    var duration = 1000;
+    var t = (performance.now() % duration) / duration;
+
+    var radius = this.size / 2 * 0.3;
+    var outerRadius = this.size / 2 * 0.7 * t + radius;
+    var context = this.context;
+
+    // draw outer circle
+    context.clearRect(0, 0, this.width, this.height);
+    context.beginPath();
+    context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
+    context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+    context.fill();
+
+    // draw inner circle
+    context.beginPath();
+    context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
+    context.fillStyle = 'rgba(255, 100, 100, 1)';
+    context.strokeStyle = 'white';
+    context.lineWidth = 2 + 4 * (1 - t);
+    context.fill();
+    context.stroke();
+
+    // update this image's data with data from the canvas
+    this.data = context.getImageData(0, 0, this.width, this.height).data;
+
+    // keep the map repainting
+    this.map.triggerRepaint();
+
+    // return `true` to let the map know that the image was updated
+    return true;
+  }
+};

8f1d9ff53a555d3946a7671e6349e43081243f84
Thibault Durand <tibo.durand@gmail.com>
2019-07-12 15:53:00 +0200

    WIP: work on navigation experience

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,11 +1,14 @@
 import { Component, h, State } from '@stencil/core';
 import mapboxgl from 'mapbox-gl';
 import { Plugins, GeolocationPosition } from '@capacitor/core';
+import { loadingController, toastController } from '@ionic/core';
 import config from '../../config.json';
 import pointGeojson from '../../assets/geojson/point.json';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 const { SplashScreen } = Plugins;
 import { PulsingDot } from '../../helpers/utils';
+import turfBbox from '@turf/bbox'
+const { Geolocation } = Plugins;
 
 /*
 
@@ -13,7 +16,10 @@ Todo include styles via module import instead of copy paste in app-home.css , wh
 
 */
 
-const { Geolocation } = Plugins;
+const MODE = {
+  SELECTING_FIELD: "selecting_field",
+  FREEMOVING: "freemoving"
+}
 
 @Component({
   tag: 'app-home',
@@ -23,11 +29,47 @@ export class AppHome {
 
   @State() position: GeolocationPosition;
   @State() positionGeojson: any = pointGeojson;
+  @State() mode: any = MODE.FREEMOVING;
+  @State() mapRendered: boolean = false;
+  @State() mapLoaded: boolean = false;
+  loadingScreen: any;
+  Draw: any;
+  map: any;
+
+  async presentToastErrorGPS() {
+    const toast = await toastController.create({
+      message: "Error while getting your location, it might be permission issue or something else, this will be handled better in M2, you can still explore the rest of the app",
+      color: 'dark',
+      position: 'top',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'cancel',
+          handler: () => {
+            if (this.loadingScreen) {
+              this.loadingScreen.dismiss();
+            }
+          }
+        }
+      ]
+    });
+
+    return await toast.present();
+  }
+
+  async presentLoading() {
+    this.loadingScreen = await loadingController.create({
+      message: 'Getting your location...',
+      duration: 15000
+    });
+
+    await this.loadingScreen.present();
+  }
 
   createPositionLayerAndSource(map) {
     let pulsingDot = new PulsingDot(map);
     map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
-    map.addSource('position' , { type: 'geojson', data: this.positionGeojson})
+    map.addSource('position', { type: 'geojson', data: this.positionGeojson })
     map.addLayer({
       "id": "position",
       "type": "symbol",
@@ -42,57 +84,121 @@ export class AppHome {
     map.getSource('position').setData(this.positionGeojson);
   }
 
+  onLocationError() {
+    this.presentToastErrorGPS();
+    if (this.loadingScreen) {
+      this.loadingScreen.dismiss();
+    }
+  }
+
+  startWatchingGeoloc() {
+    Geolocation.getCurrentPosition({
+      timeout: 15000
+    }).then((position) => {
+      if (position) {
+        if (this.loadingScreen) {
+          this.loadingScreen.dismiss();
+          console.log(this.loadingScreen);
+        }
+        this.position = position;
+        this.map.panTo({ lon: position.coords.longitude, lat: position.coords.latitude })
+      } else {
+        this.onLocationError();
+      }
+
+      setTimeout(() => {
+        if (this.loadingScreen) {
+          this.loadingScreen.dismiss();
+        }
+      }, 1000)
+
+      Geolocation.watchPosition({
+        enableHighAccuracy: true,
+        timeout: 15000
+      }, (position) => {
+        if (position) {
+          if (this.loadingScreen) {
+            this.loadingScreen.dismiss();
+          }
+          this.position = position;
+          // Update layer
+          this.positionGeojson.features[0].geometry.coordinates = [this.position.coords.longitude, this.position.coords.latitude];
+          if (!this.map.getSource("position")) {
+            this.createPositionLayerAndSource(this.map);
+          }
+          this.updatePositionInSource(this.map);
+        } else {
+          this.onLocationError();
+        }
+      })
+    }).catch((error) => {
+      console.log(error);
+      this.onLocationError();
+    });
+  }
+
+  enableSelectFieldMode() {
+    this.mode = MODE.SELECTING_FIELD;
+    this.Draw.changeMode('draw_polygon');
+  }
+
   componentDidLoad() {
+
+    this.presentLoading();
 
     SplashScreen.hide();
 
     mapboxgl.accessToken = config.mapboxToken;
 
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/satellite-v9',
-      zoom: 17
+      center: [4.7677,46.3139],
+      // style: 'mapbox://styles/mapbox/satellite-v9',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 10
     });
 
-    var Draw = new MapboxDraw();
-
-    map.addControl(Draw, 'bottom-left');
-
-    map.on('draw.create', () => {
-      console.log('blabla')
+    // Add drawing tools
+    this.Draw = new MapboxDraw({
+      displayControlsDefault: false,
+      boxSelect: false
     });
 
-    Geolocation.getCurrentPosition().then((position) => {
-      if (position) {
-        this.position = position;
-        map.panTo({ lon: position.coords.longitude, lat: position.coords.latitude })
+    this.map.on('draw.create', (feature) => {
+      var bbox = turfBbox(feature.features[0])
+      console.log(bbox);
+
+
+      // var bbox = [-95, 30 ,-85, 40];
+      // var cellSide = 50; // cell size
+      // var options = {units: 'kilometers'};
+
+      // var squareGrid = turf.squareGrid(bbox, cellSide, options);
+    });
+
+    this.map.on('render', () => {
+
+      if(this.mapRendered === false) {
+        console.log('first render')
+        this.mapRendered = true;
+        this.map.resize();
       }
-
-      Geolocation.watchPosition({
-        enableHighAccuracy: true
-      }, (position) => {
-        if (position) {
-          this.position = position;
-          // Update layer
-          this.positionGeojson.features[0].geometry.coordinates = [this.position.coords.longitude, this.position.coords.latitude];
-          if (!map.getSource("position")) {
-            this.createPositionLayerAndSource(map);
-          }
-          this.updatePositionInSource(map);
-        }
-      })
     });
 
-
-    map.on('load', () => {
-      map.resize();
+    this.map.on('load', () => {
+      console.log('map loaded')
+      this.mapLoaded = true;
+      // Add position
       this.positionGeojson.features[0].geometry.coordinates = [this.position.coords.longitude, this.position.coords.latitude];
-
-      if (!map.getSource("position")) {
-        this.createPositionLayerAndSource(map);
+      if (!this.map.getSource("position")) {
+        this.createPositionLayerAndSource(this.map);
       }
-      this.updatePositionInSource(map);
+      this.updatePositionInSource(this.map);
+
+      this.map.addControl(this.Draw);
     });
+
+    this.startWatchingGeoloc();
   }
 
   render() {
@@ -105,11 +211,23 @@ export class AppHome {
 
       <ion-content>
         <div id="map"></div>
-        <ion-fab vertical="bottom" horizontal="center" slot="fixed" style={{transform: 'scale(1.5)', bottom: '40px'}}>
-          <ion-fab-button>
-            <ion-icon name="navigate"></ion-icon>
-          </ion-fab-button>
-        </ion-fab>
+        {/* {this.mapLoaded &&
+          <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+            {this.mode === MODE.FREEMOVING &&
+              <ion-button
+                color="primary"
+                onClick={() => this.enableSelectFieldMode()}
+              >
+                Select area
+              </ion-button>
+            }
+            {this.mode === MODE.SELECTING_FIELD &&
+              <ion-fab-button>
+                <ion-icon name="checkmark"></ion-icon>
+              </ion-fab-button>
+            }
+          </ion-fab>
+        } */}
       </ion-content>
     ];
   }

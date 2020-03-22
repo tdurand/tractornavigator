41420aa0aabb9faea0188445dbed78ec3a9040dc
Thibault Durand <tibo.durand@gmail.com>
2020-03-22 11:22:57 +0100

    Evo: upgrade dependencies

--- src/statemanagement/store.ts
+++ src/statemanagement/store.ts
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 //import logger from "redux-logger";
 import rootReducer from "./reducers";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 
 let middlewares = []
